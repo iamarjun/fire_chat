aa432c02b93d1e77d41c20faa5fa22cecbc48e02
iamarjun <arjunmanoj1995@gmail.com>
2020-08-22 23:39:55 +0530

    create application class and init hilt

---
+++ app/src/main/java/com/arjun/firechat/FireChatApp.kt
@@ -0,0 +1,15 @@
+package com.arjun.firechat
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class FireChatApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
